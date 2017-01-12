d5c16c34bd474e7940688925f056c49e1c5a1966
stevenpan91 <stevenpan91@gmail.com>
2017-01-12 20:23:15 +0900

    remove scroll testing

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -8,11 +8,8 @@ void kernel_start() {
 	set_cursor(get_screen_offset(0,2));
 	print("32-Bit Protected Mode Initiated.\n");
 	print("Screen driver operational.\n");
-    /*Below happens if there are more than two char arguments in print_char method:
+    	/*Below happens if there are more than two char arguments in print_char method:
 	char (first arg): read as 0x7D no matter what
 	attr (fourth arg): off by +0x01)*/
 
-	int i;
-	for(i=0; i<160; i++){
-		print("Testscrolling");}	
 }
